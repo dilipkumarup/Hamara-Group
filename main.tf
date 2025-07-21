966b5fc94df5efb454c038f22b283941642afa5f
Ashwini Jha <kr.ashwinikumarjha@gmail.com>
2025-07-21 21:06:13 +0530

    adding one more rg

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "gittest" {
   name     = "gittest"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "gittest1" {
+  name     = "gittest1"
+  location = "West Europe"
+}
